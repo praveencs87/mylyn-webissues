168dc6163de9f7e3c8d5b01a32c048a405f19753
tanktarta <tanktarta@10a0af80-47d7-f06c-5713-db77e9087275>
2011-03-28 10:39:26 +0000

    Initial import.

---
+++ net.sf.webissues.api/src/main/java/net/sf/webissues/api/NamedEntity.java
@@ -0,0 +1,16 @@
+package net.sf.webissues.api;
+
+/**
+ * Many WebIssues entities have a 'name'. All objects that support this should
+ * implement this interface.
+ */
+public interface NamedEntity extends Entity {
+
+    /**
+     * Get the entity name.
+     * 
+     * @return name
+     */
+    String getName();
+
+}
---
+++ net.sf.webissues.api/src/test/java/net/sf/webissues/api/SaveClientTest.java
@@ -0,0 +1,35 @@
+package net.sf.webissues.api;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+
+import org.junit.Ignore;
+
+@Ignore
+public class SaveClientTest extends AbstractClientTest {
+    SaveClientTest(String[] args) {
+        super(args[1], args[2].toCharArray());
+    }
+
+    public static void main(final String[] args) throws Exception {
+        SaveClientTest lct = new SaveClientTest(args);
+        Client c = new Client();
+        c.setUrl(new URL(args[0]));
+        Operation op = lct.createOperation();
+        Authenticator authenticator2 = lct.createAuthenticator();
+        c.setAuthenticator(authenticator2);
+        c.connect(op);
+        dumpClient(c, op);
+        File file = new File(new File(System.getProperty("java.io.tmpdir")), "client.ser");
+        FileOutputStream fos = new FileOutputStream(file);
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(c);
+            oos.flush();
+        } finally {
+            fos.close();
+        }
+    }
+}
